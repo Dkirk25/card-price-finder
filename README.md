5f8ac6204076967008cf0b06b1a1ca2c69308b66
Donald Kirk <dkirk9325@gmail.com>
2021-01-10 13:44:48 -0600

    Added readme.

---
+++ README.md
@@ -0,0 +1,22 @@
+# Training Card Price Finder
+
+A Python program that takes a list of yu-gi-oh card ids and returns the market price of each card along with the name, id, rarity, and total amount.
+
+Requirements
+
+- Python >=3.7
+
+# Setup/Installation
+
+1. Fork/Clone the repository `git clone https://github.com/Dkirk25/card-price-finder.git`
+2. pip install -r requirements.txt
+3. Add list of card id to 'cards.txt'
+4. On mac, You might have to set 'OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES' in env
+5. Test run your project locally with `python robin.py`
+
+# Integrations
+- TCGPlayer.com
+  · https://shop.tcgplayer.com/yugioh?newSearch=true
+
+# PIP Modules
+View the [requirements.txt]https://github.com/Dkirk25/card-price-finder/blob/main/requirements.txt
--- main.py
+++ main.py
@@ -3,58 +3,50 @@ from bs4 import BeautifulSoup
 import string
 import time
 import  os
-import _thread
 import numpy as np
-# import multiprocessing
-from functools import partial
-from itertools import product
+import multiprocessing
 
 PARSER = 'html.parser'
 
-list_of_card_details = []
-list_of_unknown_cards = []
-
 def main():
-    import multiprocessing
-
-    global list_of_card_details
-    global list_of_unknown_cards
-
-    list_of_card_numbers = open('cards.txt', 'r').read().splitlines()
+    initial_list = open('cards.txt', 'r').read().splitlines()
+    list_of_card_numbers = list(set(initial_list))
 
     processes = []
     number_of_processes = 5
     thread_lists = np.array_split(list_of_card_numbers, number_of_processes)
     q = multiprocessing.Queue()
+    q2 = multiprocessing.Queue()
     for i in range(number_of_processes):
-        processes.append(multiprocessing.Process(target=get_card_price, args=(thread_lists[i],q)))
+        processes.append(multiprocessing.Process(target=get_card_price, args=(thread_lists[i],q,q2)))
 
     for p in processes:
         p.start()
 
     liveprocs = list(processes)
+    list_of_card_details = []
+    list_of_unknown_cards = []
     while liveprocs:
         try:
             while 1:
                 list_of_card_details.extend((q.get(False)))
+                list_of_unknown_cards.extend((q2.get(False)))
         except Exception as ex:
+            print(ex)
             pass
         time.sleep(0.5)    # Give tasks a chance to put more data in
         if not q.empty():
             continue
         liveprocs = [p for p in liveprocs if p.is_alive()]
 
-    print("threads finished....")
-
-    # both processes finished 
-    print("Both processes finished execution!") 
+    print("All processes finished execution!") 
 
     sorted_card_list = sorted(list_of_card_details, key=lambda k: float(k['price'][1:]), reverse= True) 
 
     print(len(sorted_card_list))
     print("Finish processing cards....")
     prices = []
-    f = open('final-card-values.txt', 'w')
+    f = open('final-card-list.txt', 'w')
     for card_detail in sorted_card_list:
         f.write("{0}, {3}, {1}, {2}\n".format(card_detail['name'], card_detail['rarity'].split(" ", 1)[1], card_detail['price'], card_detail['code']))
         price = card_detail["price"]
@@ -69,15 +61,12 @@ def main():
     f.close()
 
 
-def get_card_price(list_of_cards, q):
-    global list_of_unknown_cards
-
+def get_card_price(list_of_cards, q, q2):
     temp_list = []
-
+    missing_card_list = []
     for card_number in list_of_cards:
-        url = f"https://shop.tcgplayer.com/yugioh/product/show?advancedSearch=true&Number={card_number}"
+        url = 'https://shop.tcgplayer.com/yugioh/product/show?advancedSearch=true&Number={0}'.format(card_number)
         with requests.get(url, stream=True) as page:
-
             soup = BeautifulSoup(page.content, PARSER)
             try:
                 name = soup.findAll(class_= "product__name")[0].text
@@ -88,9 +77,9 @@ def get_card_price(list_of_cards, q):
                 temp_list.append(card_details)
             except Exception as ex:
                 print(ex)
-                list_of_unknown_cards.append(card_number)
+                missing_card_list.append(card_number)
     q.put(temp_list)
-    # q.put(None)
+    q2.put(missing_card_list)
     print(len(temp_list))
     
 if __name__ == '__main__':
