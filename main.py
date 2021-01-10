7da31149c372c3539dc0a155fcd9a3b56dbf75bd
Donald Kirk <dkirk9325@gmail.com>
2021-01-10 13:56:44 -0600

    refactored code.

--- main.py
+++ main.py
@@ -14,11 +14,11 @@ def main():
 
     processes = []
     number_of_processes = 5
-    thread_lists = np.array_split(list_of_card_numbers, number_of_processes)
+    card_list_split_up = np.array_split(list_of_card_numbers, number_of_processes)
     q = multiprocessing.Queue()
     q2 = multiprocessing.Queue()
     for i in range(number_of_processes):
-        processes.append(multiprocessing.Process(target=get_card_price, args=(thread_lists[i],q,q2)))
+        processes.append(multiprocessing.Process(target=get_card_price, args=(card_list_split_up[i],q,q2)))
 
     for p in processes:
         p.start()
@@ -32,7 +32,6 @@ def main():
                 list_of_card_details.extend((q.get(False)))
                 list_of_unknown_cards.extend((q2.get(False)))
         except Exception as ex:
-            print(ex)
             pass
         time.sleep(0.5)    # Give tasks a chance to put more data in
         if not q.empty():
@@ -41,24 +40,14 @@ def main():
 
     print("All processes finished execution!") 
 
+    # Sort by price
     sorted_card_list = sorted(list_of_card_details, key=lambda k: float(k['price'][1:]), reverse= True) 
 
-    print(len(sorted_card_list))
     print("Finish processing cards....")
-    prices = []
-    f = open('final-card-list.txt', 'w')
-    for card_detail in sorted_card_list:
-        f.write("{0}, {3}, {1}, {2}\n".format(card_detail['name'], card_detail['rarity'].split(" ", 1)[1], card_detail['price'], card_detail['code']))
-        price = card_detail["price"]
-        prices.append(float(price[1:]))
 
-    # print total price
-    f.write("\nTotal price worth: {0}".format(sum(prices)))
-    f.close()
+    write_final_results(sorted_card_list,'final-card-list.txt')
 
-    f = open('bad-cards.txt', 'w')
-    [f.write("{0}\n".format(number)) for number in list_of_unknown_cards]
-    f.close()
+    write_missing_cards(list_of_unknown_cards,'bad-cards.txt')
 
 
 def get_card_price(list_of_cards, q, q2):
@@ -80,7 +69,23 @@ def get_card_price(list_of_cards, q, q2):
                 missing_card_list.append(card_number)
     q.put(temp_list)
     q2.put(missing_card_list)
-    print(len(temp_list))
+
+def write_final_results(list_of_card_results,file_name):
+    prices = []
+    f = open(file_name, 'w')
+    for card_detail in list_of_card_results:
+        f.write("{0}, {3}, {1}, {2}\n".format(card_detail['name'], card_detail['rarity'].split(" ", 1)[1], card_detail['price'], card_detail['code']))
+        price = card_detail["price"]
+        prices.append(float(price[1:]))
+
+    # print total price
+    f.write("\nTotal price worth: {0}".format(sum(prices)))
+    f.close()
+
+def write_missing_cards(list_of_missing_cards, file_name):
+    f = open(file_name, 'w')
+    [f.write("{0}\n".format(number)) for number in list_of_missing_cards]
+    f.close()
     
 if __name__ == '__main__':
     main()
